0914495375d40e1a804b580f1cc1c47a16799d95
Katherine Evans <katherine@launchpadlab.com>
2021-12-23 17:34:08 -0600

    updates

--- app/controllers/api/v1/logos_controller.rb
+++ app/controllers/api/v1/logos_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::LogosController < ApplicationController
+    
     def index
-    logos = Logo.all
-    json_with logos
+        logos = Logo.all
+        logos.as_json
     end
 
     def show
-    logo = Logo.find(params[:id])
-    json_with logo
+        logo = Logo.find(params[:id])
+        logo.as_json
     end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
  
   # GET route to know that API is online.
   # Limited to json requests - sends a 200 response with empty headers and empty response body
-  root to: "logos#index"
+  root to: "logos_controller#index"
 
   namespace :api do
     namespace :v1 do
-      resources :logos
+      get '/logos', to: 'logos#index'
+      get '/logos/:id', to: 'logos#show'
     end
   end
 
