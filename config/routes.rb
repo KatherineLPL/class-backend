e231adf7748aa4ae14d9ac1d44f58ad3a6fa8036
Katherine Evans <katherine@launchpadlab.com>
2021-12-23 17:18:22 -0600

    main

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
  
   # GET route to know that API is online.
   # Limited to json requests - sends a 200 response with empty headers and empty response body
-  if Rails.env.development?
-    get '/', to: proc { [200, {}, ['']] }, constraints: { format: 'json' }
-  end
+  root to: "logos#index"
 
   namespace :api do
     namespace :v1 do
