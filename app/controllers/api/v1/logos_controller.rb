99c20302704cc5d9f3a1373ec64ab99006cab6f0
Katherine Evans <katherine@launchpadlab.com>
2021-12-23 17:35:26 -0600

    push up commits

--- app/controllers/api/v1/logos_controller.rb
+++ app/controllers/api/v1/logos_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::LogosController < ApplicationController
     
     def index
         logos = Logo.all
-        logos.as_json
+        render json: logos
     end
 
     def show
         logo = Logo.find(params[:id])
-        logo.as_json
+        render json: logo
     end 
 
 end
