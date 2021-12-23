81951faf2de07c452511d55f0ceac3678735f78d
Katherine Evans <katherine@launchpadlab.com>
2021-12-23 17:08:59 -0600

    routes, controller

--- app/controllers/api/v1/logos_controller.rb
+++ app/controllers/api/v1/logos_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::LogosController < ApplicationController
+    def index
+    logos = Logo.all
+    json_with logos
+    end
+
+    def show
+    logo = Logo.find(params[:id])
+    json_with logo
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+  # GET route to know that API is online.
+  # Limited to json requests - sends a 200 response with empty headers and empty response body
+  if Rails.env.development?
+    get '/', to: proc { [200, {}, ['']] }, constraints: { format: 'json' }
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :logos
+    end
+  end
+
 end
